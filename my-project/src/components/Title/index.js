b7200abbcf187570cd1c1c1ded62b376fee535e7
danleysilva <75344795+danleysilva@users.noreply.github.com>
2022-04-09 14:38:14 -0300

    Title added
    Title added

---
+++ my-project/src/components/Title/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { View, Text} from "react-native";
+
+export default function Title(){
+    return(
+        <View>
+            <Text>Calculadora de Média e Retorno de Intervalo de Valores</Text>
+        </View>
+
+    );
+}
