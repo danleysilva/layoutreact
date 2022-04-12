361c582a5faeb1c730f9c18178aa0131f17ecb8f
danleysilva <75344795+danleysilva@users.noreply.github.com>
2022-04-12 00:22:11 -0300

    Status return
    Status return, approver, disapproved, recovery

--- my-project/src/components/Form/ResultMedia/index.js
+++ my-project/src/components/Form/ResultMedia/index.js
@@ -6,7 +6,18 @@ export default function ResultMedia (props){
     return(
         <View style={styles.resultMedia}>
             <Text style={styles.information}>{props.messageResultMedia}</Text>
-            <Text style={styles.numberMedia}>{props.ResultMedia}</Text>
-        </View>
+           {/* <Text style={styles.numberMedia}>{props.ResultMedia}</Text> */} 
+            {props.ResultMedia > 0 && props.ResultMedia < 5 ? (
+    <Text>{props.ResultMedia} <br></br>Reprovado</Text>
+) : props.ResultMedia >= 5 && props.ResultMedia < 7 ? (
+    <Text>{props.ResultMedia}<br></br> Recuperação</Text>
+) : props.ResultMedia >= 7 ? (
+    <Text>{props.ResultMedia}<br></br> Aprovado</Text>
+) : (
+    <Text ></Text>
+)}
+
+</View>
+        
     );
 }
